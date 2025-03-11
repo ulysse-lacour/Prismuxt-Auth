8f4f69c9d507807189cbcf72110d02c45b989e48
Ulysse Lacour <contact@ulysselacour.com>
2025-03-11 15:57:50 +0100

    Data structure

--- nuxt/prisma/migrations/20250311143842_projects_and_portfolios_structure/migration.sql
+++ nuxt/prisma/migrations/20250311143842_projects_and_portfolios_structure/migration.sql
@@ -18,41 +18,6 @@ CREATE TABLE "project" (
 );
 
 -- CreateTable
-CREATE TABLE "design" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "description" TEXT,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "design_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "design_block" (
-    "id" TEXT NOT NULL,
-    "type" "BlockType" NOT NULL,
-    "order" INTEGER NOT NULL,
-    "config" JSONB NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "designId" TEXT NOT NULL,
-
-    CONSTRAINT "design_block_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "block_content" (
-    "id" TEXT NOT NULL,
-    "content" JSONB NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "designBlockId" TEXT NOT NULL,
-
-    CONSTRAINT "block_content_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "portfolio" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
@@ -74,7 +39,6 @@ CREATE TABLE "portfolio_project" (
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "portfolioId" TEXT NOT NULL,
     "projectId" TEXT NOT NULL,
-    "designId" TEXT,
 
     CONSTRAINT "portfolio_project_pkey" PRIMARY KEY ("id")
 );
@@ -84,17 +48,14 @@ CREATE TABLE "portfolio_project_block" (
     "id" TEXT NOT NULL,
     "type" "BlockType" NOT NULL,
     "order" INTEGER NOT NULL,
-    "config" JSONB NOT NULL,
-    "content" JSONB NOT NULL,
+    "config" JSONB,
+    "content" JSONB,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "portfolioProjectId" TEXT NOT NULL,
 
     CONSTRAINT "portfolio_project_block_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "block_content_designBlockId_key" ON "block_content"("designBlockId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "portfolio_slug_key" ON "portfolio"("slug");
@@ -106,12 +67,6 @@ CREATE UNIQUE INDEX "portfolio_project_portfolioId_projectId_key" ON "portfolio_
 ALTER TABLE "project" ADD CONSTRAINT "project_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "design_block" ADD CONSTRAINT "design_block_designId_fkey" FOREIGN KEY ("designId") REFERENCES "design"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "block_content" ADD CONSTRAINT "block_content_designBlockId_fkey" FOREIGN KEY ("designBlockId") REFERENCES "design_block"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "portfolio" ADD CONSTRAINT "portfolio_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
@@ -119,9 +74,6 @@ ALTER TABLE "portfolio_project" ADD CONSTRAINT "portfolio_project_portfolioId_fk
 
 -- AddForeignKey
 ALTER TABLE "portfolio_project" ADD CONSTRAINT "portfolio_project_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "portfolio_project" ADD CONSTRAINT "portfolio_project_designId_fkey" FOREIGN KEY ("designId") REFERENCES "design"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "portfolio_project_block" ADD CONSTRAINT "portfolio_project_block_portfolioProjectId_fkey" FOREIGN KEY ("portfolioProjectId") REFERENCES "portfolio_project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- nuxt/prisma/seed.mjs
+++ nuxt/prisma/seed.mjs
@@ -1,4 +1,4 @@
-import { BlockType, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
